c3d2ae39026888795dfdec157454a1d4c6f22282
yesyep <yesyeayep@gamil.com>
2017-04-04 16:12:49 +0900

    Default settings information from MySQL

--- src/Database/DBConnector.java
+++ src/Database/DBConnector.java
@@ -1,5 +1,27 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnector {
 
+	private Connection connection = null;
+
+	public Connection getConnection(){
+
+		try {
+			// MySQL Driver 로드
+			Class.forName(MySQLConnection.getDriver());
+
+			// DB 연결 설정, connection 생성.
+			connection = DriverManager.getConnection(
+					MySQLConnection.getUrl(),MySQLConnection.getUser(), MySQLConnection.getPassword());
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return connection;
+
+	}
 }
